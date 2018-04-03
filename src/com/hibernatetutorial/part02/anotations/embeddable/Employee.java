172b47c1a3544696a32b60131a09e62d2892b443
Tutku Ince <33215575+tutkuince@users.noreply.github.com>
2018-04-03 12:06:37 +0300

    Add files via upload

---
+++ src/com/hibernatetutorial/part02/anotations/embeddable/Employee.java
@@ -0,0 +1,72 @@
+package com.hibernatetutorial.part02.anotations.embeddable;
+
+import java.io.Serializable;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Employee implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	private int id;
+	private String name;
+	private String surname;
+
+	@Embedded
+	private Address address;
+
+	public Employee() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Employee(String name, String surname) {
+		super();
+		this.name = name;
+		this.surname = surname;
+	}
+
+	public Employee(int id, String name, String surname) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.surname = surname;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+}
